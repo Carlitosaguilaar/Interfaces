28b9b0f187eb2e9230c3e2793689ac3d81a48b02
Carlitosaguilaar <caa0005@alu.medac.es>
2022-01-12 11:26:12 +0100

    12/01

---
+++ Trimestre2/Tarea3/tarea3.js
@@ -0,0 +1,13 @@
+function cambioshow() {
+
+    var dedo = document.getElementsByClassName("Cajaflotante");
+    dedo.style.display = "block";
+
+    if (dedo.style.display == "block") {
+        dedo.style.display = "none"
+    }
+    else{
+        dedo.style.display = "none"
+    }
+    
+}
