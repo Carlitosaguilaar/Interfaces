50c63c3611e0378aaede7c9904ef7d44acd1e178
Carlitosaguilaar <caa0005@alu.medac.es>
2022-01-14 13:08:30 +0100

    Fin tarea 3

--- Trimestre2/Tarea3/tarea3.js
+++ Trimestre2/Tarea3/tarea3.js
@@ -1,16 +1,11 @@
 function cambiodisplay() {
 
-    var dedo = document.querySelector("section");
+    var dedo = document.getElementById("Cajaflotante");
 
-    var dedo2 = document.getElementById("Cajabase")
-
-    if (dedo.style.display == "none" && dedo2.style.display == "block") {
-        dedo.style.display = "block";
-        dedo2.style.display = "none";
+    if (dedo.style.display == "none") {
+        dedo.style.display = "block"
     }
-
-    if (dedo.style.display == "block" || dedo2.style.display == "none") {
-        dedo.style.display = "none";
-        dedo2.style.display = "block";
+    else{
+        dedo.style.display = "none"
     }
 }
